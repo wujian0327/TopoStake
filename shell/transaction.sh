9b0199aa4a558a65a0fb9421e9c63ae13da89b3e
sqdfa <2926005474@qq.com>
2026-03-01 10:30:31 +0800

    shell

---
+++ shell/transaction.sh
@@ -0,0 +1,27 @@
+cargo run --release -- -n 100 -t 50  -c topostake -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 125 --max-epochs 100
+cargo run --release -- -n 100 -t 100 -c topostake -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 250 --max-epochs 100
+cargo run --release -- -n 100 -t 150 -c topostake -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 375 --max-epochs 100
+cargo run --release -- -n 100 -t 200 -c topostake -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 500 --max-epochs 100
+cargo run --release -- -n 100 -t 250 -c topostake -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 625 --max-epochs 100
+cargo run --release -- -n 100 -t 300 -c topostake -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 750 --max-epochs 100
+
+cargo run --release -- -n 100 -t 50  -c pos -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 125 --max-epochs 100
+cargo run --release -- -n 100 -t 100 -c pos -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 250 --max-epochs 100
+cargo run --release -- -n 100 -t 150 -c pos -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 375 --max-epochs 100
+cargo run --release -- -n 100 -t 200 -c pos -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 500 --max-epochs 100
+cargo run --release -- -n 100 -t 250 -c pos -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 625 --max-epochs 100
+cargo run --release -- -n 100 -t 300 -c pos -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 750 --max-epochs 100
+
+cargo run --release -- -n 100 -t 50  -c pow -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 125 --max-epochs 100
+cargo run --release -- -n 100 -t 100 -c pow -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 250 --max-epochs 100
+cargo run --release -- -n 100 -t 150 -c pow -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 375 --max-epochs 100
+cargo run --release -- -n 100 -t 200 -c pow -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 500 --max-epochs 100
+cargo run --release -- -n 100 -t 250 -c pow -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 625 --max-epochs 100
+cargo run --release -- -n 100 -t 300 -c pow -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 750 --max-epochs 100
+
+cargo run --release -- -n 100 -t 50  -c minotaur -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 125 --max-epochs 100
+cargo run --release -- -n 100 -t 100 -c minotaur -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 250 --max-epochs 100
+cargo run --release -- -n 100 -t 150 -c minotaur -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 375 --max-epochs 100
+cargo run --release -- -n 100 -t 200 -c minotaur -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 500 --max-epochs 100
+cargo run --release -- -n 100 -t 250 -c minotaur -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 625 --max-epochs 100
+cargo run --release -- -n 100 -t 300 -c minotaur -g 0.6 --base-reward 1.0 --slot-duration 2 --transaction-fee 0.00001 --max-tx-per-block 750 --max-epochs 100
